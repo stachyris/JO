61c61e900577e4872fa683ee730ad7682fc8aa89
meghananatesh <47441735+meghananatesh@users.noreply.github.com>
2020-03-03 14:20:17 +0530

    Create map_fq_2_ref.sh

---
+++ map_fq_2_ref.sh
@@ -0,0 +1,2 @@
+/home/Workshop-Genomics/softs/bwa-0.7.17/bwa mem -t 32 -p -M /home/harshakumar/Meghana_New_Dir/Reference_Genomes_NCBI/GCF_003259725.1_athCun1_genomic.fna \
+  JO1.adaptersmarked.fastq > JO1.adaptersmarked.aligned.sam
