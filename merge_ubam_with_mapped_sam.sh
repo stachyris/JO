5e3babc3c8eb535b408889cb72dd6070531dcb09
meghananatesh <47441735+meghananatesh@users.noreply.github.com>
2020-03-03 14:22:23 +0530

    Delete merge_ubam_with_mapped_sam.sh

--- merge_ubam_with_mapped_sam.sh
+++
@@ -1,8 +0,0 @@
-java -jar /home/Workshop-Genomics/softs/picard.jar MergeBamAlignment \
-  ALIGNED_BAM=JO1.adaptersmarked.aligned.sam \
-  UNMAPPED_BAM=JO1.adaptersmarked_unmapped.bam \
-  OUTPUT=JO1.adaptersmarked.aligned.merged.bam \
-  REFERENCE_SEQUENCE=/home/harshakumar/Meghana_New_Dir/Reference_Genomes_NCBI/GCF_003259725.1_athCun1_genomic.fna \
-  MAX_INSERTIONS_OR_DELETIONS=-1 \
-  PRIMARY_ALIGNMENT_STRATEGY=MostDistant \
-  ATTRIBUTES_TO_RETAIN=XS
